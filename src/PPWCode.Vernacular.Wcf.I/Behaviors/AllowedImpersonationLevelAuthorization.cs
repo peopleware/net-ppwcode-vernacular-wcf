6640786ec64eaa7f471584c301524f712554890a
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2014-05-19 18:39:42 +0000

    - Add ability to change the AllowedImpersonationLevelAuthorization before creating the channelfactory - Add ability to change the PrincipalPermissionMode before opening the servicehost
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Wcf.I/dev/PPWCode.Vernacular.Wcf.I@7599 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ src/PPWCode.Vernacular.Wcf.I/Behaviors/AllowedImpersonationLevelAuthorization.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Principal;
+using System.ServiceModel;
+
+using Castle.Facilities.WcfIntegration;
+
+namespace PPWCode.Vernacular.Wcf.I.Behaviors
+{
+    public class AllowedImpersonationLevelAuthorization : AbstractChannelFactoryAware
+    {
+        private readonly TokenImpersonationLevel m_TokenImpersonationLevel;
+
+        public AllowedImpersonationLevelAuthorization(TokenImpersonationLevel tokenImpersonationLevel)
+        {
+            m_TokenImpersonationLevel = tokenImpersonationLevel;
+        }
+
+        /// <inheritdoc/>
+        public override void Opening(ChannelFactory channelFactory)
+        {
+            base.Opening(channelFactory);
+
+            if (channelFactory.Credentials != null)
+            {
+                channelFactory.Credentials.Windows.AllowedImpersonationLevel = m_TokenImpersonationLevel;
+            }
+        }
+    }
+}
---
+++ src/PPWCode.Vernacular.Wcf.I/Behaviors/PrincipalPermissionModeAuthorization.cs
@@ -0,0 +1,37 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Description;
+
+using Castle.Facilities.WcfIntegration;
+
+namespace PPWCode.Vernacular.Wcf.I.Behaviors
+{
+    public class PrincipalPermissionModeAuthorization : AbstractServiceHostAware
+    {
+        private readonly PrincipalPermissionMode m_PrincipalPermissionMode;
+
+        public PrincipalPermissionModeAuthorization(PrincipalPermissionMode principalPermissionMode)
+        {
+            m_PrincipalPermissionMode = principalPermissionMode;
+        }
+
+        protected override void Opening(ServiceHost serviceHost)
+        {
+            ServiceAuthorizationBehavior authorization = EnsureServiceBehavior<ServiceAuthorizationBehavior>(serviceHost);
+            authorization.PrincipalPermissionMode = m_PrincipalPermissionMode;
+        }
+
+        private static T EnsureServiceBehavior<T>(ServiceHost serviceHost)
+            where T : class, IServiceBehavior, new()
+        {
+            T behavior = serviceHost.Description.Behaviors.Find<T>();
+
+            if (behavior == null)
+            {
+                behavior = new T();
+                serviceHost.Description.Behaviors.Add(behavior);
+            }
+
+            return behavior;
+        }
+    }
+}
