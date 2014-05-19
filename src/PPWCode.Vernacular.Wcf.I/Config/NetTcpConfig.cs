788fc33fe7b31ee46815223a4beef51fcf6e9e5c
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2014-05-19 10:41:28 +0000

    Add NetTcpBinding configuration
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Wcf.I/dev/PPWCode.Vernacular.Wcf.I@7593 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/PPWCode.Vernacular.Wcf.I/Config/NetTcpConfig.cs
+++ src/PPWCode.Vernacular.Wcf.I/Config/NetTcpConfig.cs
@@ -14,21 +14,23 @@ namespace PPWCode.Vernacular.Wcf.I.Config
     public sealed class NetTcpConfig<T>
         where T : class
     {
-        private const string DefaultHost = "localhost";
-        private const string DefaultInactivityTimeout = "00:10:00";
-        private const string DefaultSendTimeout = "00:01:00";
-        private const string DefaultReceiveTimeout = "00:10:00";
-        private const string DefaultOpenTimeout = "00:01:00";
-        private const string DefaultCloseTimeout = "00:01:00";
-        private const int DefaultMaxBufferSize = 65536;
-        private const int DefaultMaxBufferPoolSize = 512 * 1024;
-        private const int DefaultMaxReceivedMessageSize = 65536;
-        private const int DefaultMaxArrayLength = 16384;
-        private const int DefaultMaxDepth = 32;
-        private const int DefaultMaxStringContentLength = 8192;
-        private const int DefaultMaxBytesPerRead = 4096;
-        private const int DefaultMaxNameTableCharCount = 16384;
-        private readonly int m_DefaultPort;
+        private const string HostKey = "Host";
+        private const string PortKey = "Port";
+        private const string ServicePrincipalNameKey = "ServicePrincipalName";
+        private const string InactivityTimeoutKey = "InactivityTimeout";
+        private const string SendTimeoutKey = "SendTimeout";
+        private const string ReceiveTimeoutKey = "ReceiveTimeout";
+        private const string OpenTimeoutKey = "OpenTimeout";
+        private const string CloseTimeoutKey = "CloseTimeout";
+        private const string MaxBufferSizeKey = "MaxBufferSize";
+        private const string MaxBufferPoolSizeKey = "MaxBufferPoolSize";
+        private const string MaxReceivedMessageSizeKey = "MaxReceivedMessageSize";
+        private const string MaxArrayLengthKey = "MaxArrayLength";
+        private const string MaxDepthKey = "MaxDepth";
+        private const string MaxStringContentLengthKey = "MaxStringContentLength";
+        private const string MaxBytesPerReadKey = "MaxBytesPerRead";
+        private const string MaxNameTableCharCountKey = "MaxNameTableCharCount";
+
         private readonly string m_Namespace;
 
         static NetTcpConfig()
@@ -39,20 +41,15 @@ namespace PPWCode.Vernacular.Wcf.I.Config
             }
         }
 
-        public NetTcpConfig(string @namespace, int defaultPort)
+        public NetTcpConfig(string @namespace)
         {
             if (@namespace == null)
             {
                 throw new ArgumentNullException("namespace");
             }
-            if (defaultPort < 1024)
-            {
-                throw new ArgumentOutOfRangeException("defaultPort", "Only port's higher then 1024 are allowed");
-            }
             Contract.EndContractBlock();
 
             m_Namespace = @namespace;
-            m_DefaultPort = defaultPort;
         }
 
         private string ServiceName
@@ -60,88 +57,163 @@ namespace PPWCode.Vernacular.Wcf.I.Config
             get { return typeof(T).Name; }
         }
 
+        private int DefaultPort
+        {
+            get { return ConfigHelper.GetAppSetting<int>(PortKey); }
+        }
+
         private int Port
         {
-            get { return GetAppSetting("Port", DefaultPort); }
+            get { return GetAppSetting(PortKey, DefaultPort); }
+        }
+
+        private string DefaultInactivityTimeout
+        {
+            get { return ConfigHelper.GetAppSetting(InactivityTimeoutKey, "00:10:00"); }
         }
 
         private TimeSpan InactivityTimeout
         {
-            get { return GetTimeout("InactivityTimeout", DefaultInactivityTimeout); }
+            get { return GetTimeout(InactivityTimeoutKey, DefaultInactivityTimeout); }
+        }
+
+        private string DefaultSendTimeout
+        {
+            get { return ConfigHelper.GetAppSetting(SendTimeoutKey, "00:01:00"); }
         }
 
         private TimeSpan SendTimeout
         {
-            get { return GetTimeout("SendTimeout", DefaultSendTimeout); }
+            get { return GetTimeout(SendTimeoutKey, DefaultSendTimeout); }
+        }
+
+        private string DefaultReceiveTimeout
+        {
+            get { return ConfigHelper.GetAppSetting(ReceiveTimeoutKey, "00:10:00"); }
         }
 
         private TimeSpan ReceiveTimeout
         {
-            get { return GetTimeout("ReceiveTimeout", DefaultReceiveTimeout); }
+            get { return GetTimeout(ReceiveTimeoutKey, DefaultReceiveTimeout); }
+        }
+
+        private string DefaultOpenTimeout
+        {
+            get { return ConfigHelper.GetAppSetting(OpenTimeoutKey, "00:01:00"); }
         }
 
         private TimeSpan OpenTimeout
         {
-            get { return GetTimeout("OpenTimeout", DefaultOpenTimeout); }
+            get { return GetTimeout(OpenTimeoutKey, DefaultOpenTimeout); }
+        }
+
+        private string DefaultCloseTimeout
+        {
+            get { return ConfigHelper.GetAppSetting(CloseTimeoutKey, "00:01:00"); }
         }
 
         private TimeSpan CloseTimeout
         {
-            get { return GetTimeout("CloseTimeout", DefaultCloseTimeout); }
+            get { return GetTimeout(CloseTimeoutKey, DefaultCloseTimeout); }
+        }
+
+        private int DefaultMaxBufferSize
+        {
+            get { return GetAppSetting(MaxBufferSizeKey, 65536); }
         }
 
         private int MaxBufferSize
         {
-            get { return GetInteger("MaxBufferSize", DefaultMaxBufferSize); }
+            get { return GetAppSetting(MaxBufferSizeKey, DefaultMaxBufferSize); }
+        }
+
+        private int DefaultMaxBufferPoolSize
+        {
+            get { return GetAppSetting(MaxBufferPoolSizeKey, 512 * 1024); }
         }
 
         private int MaxBufferPoolSize
         {
-            get { return GetInteger("MaxBufferPoolSize", DefaultMaxBufferPoolSize); }
+            get { return GetAppSetting(MaxBufferPoolSizeKey, DefaultMaxBufferPoolSize); }
+        }
+
+        private int DefaultMaxReceivedMessageSize
+        {
+            get { return GetAppSetting(MaxReceivedMessageSizeKey, 65536); }
         }
 
         private int MaxReceivedMessageSize
         {
-            get { return GetInteger("MaxReceivedMessageSize", DefaultMaxReceivedMessageSize); }
+            get { return GetAppSetting(MaxReceivedMessageSizeKey, DefaultMaxReceivedMessageSize); }
+        }
+
+        private int DefaultMaxArrayLength
+        {
+            get { return GetAppSetting(MaxArrayLengthKey, 16384); }
         }
 
         private int MaxArrayLength
         {
-            get { return GetInteger("MaxArrayLength", DefaultMaxArrayLength); }
+            get { return GetAppSetting(MaxArrayLengthKey, DefaultMaxArrayLength); }
+        }
+
+        private int DefaultMaxDepth
+        {
+            get { return GetAppSetting(MaxDepthKey, 32); }
         }
 
         private int MaxDepth
         {
-            get { return GetInteger("MaxDepth", DefaultMaxDepth); }
+            get { return GetAppSetting(MaxDepthKey, DefaultMaxDepth); }
+        }
+
+        private int DefaultMaxStringContentLength
+        {
+            get { return GetAppSetting(MaxStringContentLengthKey, 8192); }
         }
 
         private int MaxStringContentLength
         {
-            get { return GetInteger("MaxStringContentLength", DefaultMaxStringContentLength); }
+            get { return GetAppSetting(MaxStringContentLengthKey, DefaultMaxStringContentLength); }
+        }
+
+        private int DefaultMaxBytesPerRead
+        {
+            get { return GetAppSetting(MaxBytesPerReadKey, 4096); }
         }
 
         private int MaxBytesPerRead
         {
-            get { return GetInteger("MaxBytesPerRead", DefaultMaxBytesPerRead); }
+            get { return GetAppSetting(MaxBytesPerReadKey, DefaultMaxBytesPerRead); }
+        }
+
+        private int DefaultMaxNameTableCharCount
+        {
+            get { return GetAppSetting(MaxNameTableCharCountKey, 16384); }
         }
 
         private int MaxNameTableCharCount
         {
-            get { return GetInteger("MaxNameTableCharCount", DefaultMaxNameTableCharCount); }
+            get { return GetAppSetting(MaxNameTableCharCountKey, DefaultMaxNameTableCharCount); }
+        }
+
+        private string DefaultHost
+        {
+            get { return ConfigHelper.GetAppSetting(HostKey, "localhost"); }
         }
 
         private string Host
         {
             get
             {
-                string host = GetAppSetting("Host", DefaultHost);
+                string host = GetAppSetting<string>(HostKey, null);
                 return string.IsNullOrWhiteSpace(host) ? DefaultHost : host;
             }
         }
 
         private string ServicePrincipalName
         {
-            get { return GetAppSetting("ServicePrincipalName", (string)null); }
+            get { return GetAppSetting(ServicePrincipalNameKey, (string)null); }
         }
 
         private string BaseAddress
@@ -214,11 +286,6 @@ namespace PPWCode.Vernacular.Wcf.I.Config
             get { return m_Namespace; }
         }
 
-        private int DefaultPort
-        {
-            get { return m_DefaultPort; }
-        }
-
         private TValue GetAppSetting<TValue>(string key, TValue defaultValue)
             where TValue : IConvertible
         {
@@ -234,11 +301,6 @@ namespace PPWCode.Vernacular.Wcf.I.Config
                 timeSpan = TimeSpan.Parse(defaultTimeout);
             }
             return timeSpan;
-        }
-
-        private int GetInteger(string key, int defaultInt)
-        {
-            return ConfigHelper.GetAppSetting(string.Concat(ServiceName, "_", key), defaultInt);
         }
 
         public IWcfClientModel GetClientModel(params object[] extensions)
