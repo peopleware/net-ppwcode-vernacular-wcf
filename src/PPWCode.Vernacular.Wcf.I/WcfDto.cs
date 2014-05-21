c64871aae35028154210b7aa825de09b6ac4a3fb
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2014-05-21 13:48:19 +0000

    Add WcfDto with IExtensibleDataObject support
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Wcf.I/dev/PPWCode.Vernacular.Wcf.I@7606 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ src/PPWCode.Vernacular.Wcf.I/WcfDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PPWCode.Vernacular.Wcf.I
+{
+    [DataContract(IsReference = true), Serializable]
+    public class WcfDto : IExtensibleDataObject
+    {
+        /// <summary>
+        /// Gets or sets the structure that contains extra data.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Runtime.Serialization.ExtensionDataObject"/> that contains data that is not recognized as belonging to the data contract.
+        /// </returns>
+        public ExtensionDataObject ExtensionData { get; set; }
+    }
+}
